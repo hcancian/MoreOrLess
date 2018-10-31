c5f3087ae3e4860528b6ef7b0d2fb4481a6bb2b3
hadrien <hadriencancian@gmail.com>
2018-10-31 12:57:03 +0100

    new ia mastermin functional but bug on the matches in mastermind

--- src/main/java/MasterMind/GameModeM.java
+++ src/main/java/MasterMind/GameModeM.java
@@ -8,9 +8,9 @@ public class GameModeM {
 
     public static void Mastermind() {
         String code;
-        int nbChiffres = 7;
+        int nbChiffres = 5;
         int MAX = 9;
-        int maxTry = 20;
+        int maxTry = 5;
         int[] codeEntre = new int[nbChiffres];
         int[] solution = new int[nbChiffres];
         int count = 0;
@@ -62,73 +62,61 @@ public class GameModeM {
             scanner.close();
         }
     }
-
-
     public static void MastermindDefense() {
-        int nbChiffres = 10;
+        int nbChiffres = 5;
         int MAX = 9;
         int maxTry = 20;
         int[] codeEntre = new int[nbChiffres];
         int[] solution = new int[nbChiffres];
-        //String solutin = "192837465";
-        // int[] solution = new int[nbChiffres];
         int count = 0;
         int i;
         int h;
-        //System.out.println(Arrays.toString(solution));
-        //int s = solutin.length();
+        int[] iaFind = new int[nbChiffres];
         Random r = new Random();
         for (i = 0; i < nbChiffres; i++) {
             solution[i] = r.nextInt(MAX + 1);
         }
-        // System.out.println(Arrays.toString(solution));
+        System.out.println(Arrays.toString(solution));
         List<Integer> goodMatches = new ArrayList<>();
         List<Integer> approximativeMatches = new ArrayList<>();
         List<Integer> wrongNumber = new ArrayList<>();
-        //for (i = 0; i < s; i++) {
-        // solution[i] = Integer.parseInt(String.valueOf(solutin.charAt(i)));
-        //}
-        System.out.println(Arrays.toString(solution));
         do {
-            for (i = 0; i < nbChiffres; i++) {
-                for (h = 0; h < 10; h++) {
-                    boolean foundMatch = false;
-                    for (int j = 0; j < nbChiffres; j++) {
-                        if (h == solution[j] && i != j && !goodMatches.contains(j)) {
-                            approximativeMatches.add(i);
-                            foundMatch = true;
-                            break;
-
-                        } else {
-                            if (h == solution[i]) {
-                                goodMatches.add(i);
-                                codeEntre[i] = h;
-                                System.out.println(Arrays.toString(codeEntre));
-
-                            }
-                            if (!foundMatch) wrongNumber.add(i);
-                        }
-                    }
-                }
-                // System.out.println(Arrays.toString(codeEntre));
+            // verif bien , mal place et non */
+              for (i = 0; i < nbChiffres; i++) {
+                      codeEntre[i] = r.nextInt(MAX + 1);
+                  if (codeEntre[i] == solution[i]) {
+                      iaFind[i] = codeEntre[i];
+                      goodMatches.add(i);
+                  } else {
+                      boolean foundMatch = false;
+                      for (int j = 0; j < nbChiffres; j++) {
+                          if (codeEntre[i] == solution[j] && i != j && !goodMatches.contains(j)) {
+                              approximativeMatches.add(i);
+                              foundMatch = true;
+                              break;
+                          }
+                      }
+                      if (!foundMatch) wrongNumber.add(i);
+                  }
+              }
+              System.out.println(Arrays.toString(codeEntre));
+              System.out.println(Arrays.toString(iaFind));
                 count++;
-                if (goodMatches.size() == nbChiffres)
-                    System.out.println("Vous avez tout trouvé");
-                else {
+                //if (goodMatches.size() == nbChiffres)
+                  //  System.out.println("Vous avez tout trouvé");
+                //else {
                     System.out.println("Il y a " + goodMatches.size() + "bons chiffres bien placés et "
                             + approximativeMatches.size() + "bon chiffre mal placés et " + wrongNumber.size()
                             + " mauvais chiffre");
-                }
+                //}
+            wrongNumber.clear();
+            approximativeMatches.clear();
+            goodMatches.clear();
+        }while (!Arrays.toString(iaFind).equals(Arrays.toString(solution)) && count <= maxTry && Arrays.toString(iaFind) != null);
+        if (Arrays.toString(iaFind).equals(Arrays.toString(solution))) {
+                System.out.println("vous avez trouvé le code en seulement " + count + " essais");
+            } else {
+                System.out.println("vous n'avez  pas trouvé le code");
             }
-        } while (!Arrays.toString(codeEntre).
-
-                equals(Arrays.toString(solution)) && count <= maxTry);
-        if (Arrays.toString(codeEntre).
-
-                equals(Arrays.toString(solution))) {
-            System.out.println("vous avez trouvé le code en seulement " + count + " essais");
-        } else {
-            System.out.println("vous n'avez  pas trouvé le code");
         }
     }
-}
